7249dde35ad499b09595c0ce40d1482bd8459386
jimin326 <151506984+jimin326@users.noreply.github.com>
2024-03-05 17:30:44 +0900

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @GetMapping("/")
     public String home() {
-        return "Hello fxxxxxx Jenkins!";
+        return "Hello fxxxxxx ddddddddddddddd Jenkins!";
     }
 }
